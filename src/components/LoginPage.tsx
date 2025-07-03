1738ab08472c518dc66cdba4b91173315c07cfcf
freakact.js <62394230+tumulakivan@users.noreply.github.com>
2025-07-04 05:18:00 +0800

    adjusted layout and removed unused image import

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 
-import fingerprint from "../assets/icons/fingerprint48.png";
 import locked from "../assets/icons/locked48.png";
 import unlocked from "../assets/icons/altunlocked48.png";
 
@@ -9,22 +8,18 @@ const LoginPage: React.FC = () => {
   const [btnStatus, setBtnStatus] = useState<boolean>(false);
 
   return (
-    <div className="flex flex-col items-center bg-gradient-to-tl from-bg to-cardend h-fit w-fit rounded-2xl p-8 shadow-login shadow-loginbg">
-      <div className="h-1/2 w-full flex items-center justify-center relative">
+    <div className="flex flex-col items-center bg-gradient-to-tl from-bg to-cardend h-[60%] w-[30%] rounded-2xl p-8 shadow-login shadow-loginbg">
+      <div className="h-1/2 w-full flex flex-col items-center justify-center">
         <img
           src="/snaplogo.png"
           alt="company logo"
-          className="w-[10rem] -translate-x-4"
+          className="w-[10rem] -translate-y-4"
         />
-        <h1 className="text-emphasizedtext text-4xl font-company -translate-x-6">
+        <h1 className="text-emphasizedtext text-4xl font-company">
           SNAPSURVEY
         </h1>
       </div>
       <div className="flex flex-col justify-center items-center h-1/2 w-full gap-4">
-        {/* <div className="flex flex-row w-full gap-2">
-          <img src={fingerprint} alt="fingerprint" className="w-6" />
-          <h2 className="text-offwhite text-xl">Login</h2>
-        </div> */}
         <form className="flex flex-col w-full gap-4">
           <input
             type="text"
@@ -39,14 +34,17 @@ const LoginPage: React.FC = () => {
             />
             <img
               src={btnHovered || btnStatus ? unlocked : locked}
-              alt="locked"
+              alt={btnStatus ? "unlocked" : "locked"}
               className="absolute w-[1.5rem] right-2 top-1/5 cursor-pointer"
               onMouseEnter={() => setBtnHovered(true)}
               onMouseLeave={() => setBtnHovered(false)}
               onClick={() => setBtnStatus(!btnStatus)}
             />
           </div>
-          <button className="w-full px-4 py-2 bg-gradient-to-tr from-btn-start via-btn-stop to-btn-end hover:outline hover:outline-emphasizedtext hover:outline-offset-1 rounded-lg cursor-pointer">
+          <button
+            type="submit"
+            className="w-full px-4 py-2 bg-gradient-to-tr from-btn-start via-btn-stop to-btn-end hover:outline hover:outline-emphasizedtext hover:outline-offset-1 rounded-lg cursor-pointer"
+          >
             Sign in
           </button>
           <div className="flex flex-row justify-between items-center w-full h-fit">
