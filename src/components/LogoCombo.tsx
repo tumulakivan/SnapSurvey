4479db8d5b8b118fb53619f726071d1501c63bd5
freakact.js <62394230+tumulakivan@users.noreply.github.com>
2025-07-05 19:13:17 +0800

    todo: implement menu bar

--- src/components/LogoCombo.tsx
+++ src/components/LogoCombo.tsx
@@ -2,11 +2,17 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import inactiveBurger from "../assets/icons/inactiveburger.png";
 import activeBurger from "../assets/icons/activeburger.png";
+import type { MenuTypes } from "../types/MenuTypes";
 
-const LogoCombo: React.FC = () => {
+const LogoCombo: React.FC<MenuTypes> = ({ onClick }) => {
   const [burgerStatus, setBurgerStatus] = useState(false);
   const [burgerHovered, setBurgerHovered] = useState(false);
   const navigate = useNavigate();
+
+  const handleBurger = () => {
+    setBurgerStatus(!burgerStatus);
+    onClick();
+  };
 
   return (
     <div className="flex flex-row items-center">
@@ -20,7 +26,7 @@ const LogoCombo: React.FC = () => {
         src={burgerStatus || burgerHovered ? activeBurger : inactiveBurger}
         alt="menu burger"
         className="w-[1.5%] -ml-[10px] cursor-pointer"
-        onClick={() => setBurgerStatus(!burgerStatus)}
+        onClick={handleBurger}
         onMouseEnter={() => setBurgerHovered(true)}
         onMouseLeave={() => setBurgerHovered(false)}
       />
